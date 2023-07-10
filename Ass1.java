5f8aa66a47d933f4b3bbc036a1868b10b4f55dc9
Ian Hou <z5324093@ad.unsw.edu.au>
2023-07-10 20:51:58 +1000

    add concurrency features

--- Ass1.java
+++ Ass1.java
@@ -1,57 +1,75 @@
 import java.util.ArrayList;
+import java.util.concurrent.Semaphore;
 /**
  * ass1
  */
 public class Ass1 {
 
     // size of bounded arr
-    static int N;
+    static int N = 5;   // this can be modified, currently set to 5 for test purposes
     // num of actual element in the array
-    static int numV;
+    static int numV = 0;
     // last ele index
-    static int lastEleIndex;
+    static int lastEleIndex = 0;
 
     // the array containing data
     static ArrayList<Integer> arr = new ArrayList<Integer>();
 
-    // constructor
-    public Ass1(int n) {
-        N = n;
-        numV = 0;
-        lastEleIndex = 0;
+    // an array of semphores that each controls writer and reader to a single cell, i.e semArr[0] manage arr[0]'s reads and writes.
+    static ArrayList<WriterReaderLock> semArr = new ArrayList<>();
+    
+    // a semphore to control the array so there can not be above N elements inside the array
+    static Semaphore arrLockSem = new Semaphore(N, true); 
+
+    // a lock that each writer operations(insert and delete in this case) needs to acquire before they do any operation.
+    static Semaphore insertDeletelockSem = new Semaphore(1, true);
+
+    // // constructor
+    public Ass1() {
+        if (semArr.size() == 0) {
+            for (int i = 0; i < N; i++) {
+                semArr.add(new WriterReaderLock());
+            }
+        }
     }
 
-    public void insert(int x) {
-        // check if the array is full
-        if (numV == N) {
-            return;
-        }
+    public void insert(int x) throws InterruptedException{
+        // acquire the array semaphore to start inserting, otherwise pending
+        arrLockSem.acquire();
         
-        // check if the array is empty
+        // acquire the insertDeleteLock to modify the array
+        insertDeletelockSem.acquire();
+
+        // case 1: the array is empty
         if (numV == 0) {
             arr.add(x);
             lastEleIndex = 0;
             numV++;
+            insertDeletelockSem.release();
             return;
         }
 
         int index = binarySearch(x);
-
-        // check duplication
-        if (arr.get(index) == x) {
-            return;
-        }
+        signalFinishReading(index, index);
 
         if(index > lastEleIndex && lastEleIndex < N - 1) {
             arr.add(x);
             lastEleIndex++;
             numV++;
+            insertDeletelockSem.release();
+            return;
+        }
+
+        // case 2: duplication
+        if (arr.get(index) == x) {
+            arrLockSem.release();
+            insertDeletelockSem.release();
             return;
         }
         
-
         int emptyIndex = findNearestEmpty(index);
-        // check if there exists empty cell
+        
+        // case 3: shift to nearest empty cell
         if (emptyIndex != -1) {
             // do the shift
             if (arr.get(index) < x && emptyIndex > index) {
@@ -59,22 +77,103 @@ public class Ass1 {
             } else if (arr.get(index) > x && emptyIndex < index) {
                 index--;
             }
+            if (index >= emptyIndex) {
+                signalWantWriting(emptyIndex, index);
+            } else {
+                signalWantWriting(index, emptyIndex);
+            }
             shift(emptyIndex, index);
-
             arr.set(index, x);
             numV++;
+            if (index >= emptyIndex) {
+                signalFinishWriting(emptyIndex, index);
+            } else {
+                signalFinishWriting(index, emptyIndex);
+            }
+            insertDeletelockSem.release();
             return;
         }
 
+        // case 4: normal shift
         arr.add(-1);
         lastEleIndex++;
+
+        // do the shift
+        signalWantWriting(index, lastEleIndex);
         shift(lastEleIndex ,index);
         arr.set(index, x);
         numV++;
-
+        signalFinishWriting(index, lastEleIndex);
+        insertDeletelockSem.release();
     }
+
+    public void delete(int x) throws InterruptedException  {
+        // acquire the insertDeltelock to modify the array
+        insertDeletelockSem.acquire();
+
+        int result = this.binarySearch(x);
+        signalFinishReading(result, result);
+        if (arr.get(result) == x) {
+            arr.set(result, -1);
+            numV--;
+        }
+        
+        // check if a compact is needed
+        if (arr.size()/numV > 1) {
+            compact();
+        } else {
+            // if not compact, release the array lock once
+            arrLockSem.release();
+        }
+        
+        // after deletion, release the writer lock
+        insertDeletelockSem.release();
+    }
+
     
 
+    public boolean member(int x) throws InterruptedException {
+        int result = this.binarySearch(x);
+    
+        if (result > lastEleIndex || result < 0) {
+            signalFinishReading(result, result);
+            return false;
+        }
+
+        if (arr.get(result) != x) {
+            signalFinishReading(result, result);
+            return false;
+        }
+        signalFinishReading(result, result);
+        return true;
+    }
+
+    public void print_sorted() throws InterruptedException {
+        signalWantReading(0, lastEleIndex);
+        String result = "";
+        result = result + "{";
+        int index = 0;
+        for (int ele : arr) {
+            if (ele == -1) {
+                continue;
+            }
+            result = result + Integer.toString(ele);
+            if (index < lastEleIndex) {
+                result = result + ", ";
+            }
+            signalFinishReading(index, index);
+            index++;
+        }
+        result = result + "}";
+        System.out.println(result);
+    }
+
+
+    /*
+     *  helper functions
+     */
+    
+    // shift the array elements from fromIndex to emptyIndex
     private void shift(int emptyIndex, int fromIndex) {
         // check the direction of the shift
         if (emptyIndex < fromIndex) {
@@ -140,7 +239,7 @@ public class Ass1 {
     }
 
     // compact the array when there are too many empty slots(i.e -1 inside the array)
-    public void compact() {
+    public void compact() throws InterruptedException {
         // arr is null, do nothing
         if (numV == 0) {
             return;
@@ -148,25 +247,31 @@ public class Ass1 {
         if (numV == 1 && arr.get(0) != -1) {
             return;
         }
+        // signal this thread wants to modify whole array
+        signalWantWriting(0, lastEleIndex);
+        
+        int temp_last = lastEleIndex;
         arr.removeIf(n -> (n == -1));
         lastEleIndex = arr.size() - 1;
-        
-    }
 
-    public void delete(int x) {
-        int result = this.binarySearch(x);
-        if (arr.get(result) == x) {
-            arr.set(result, -1);
-            numV--;
+        // release arrLockSem the amount of time equal to the empty element got deleted
+        for (int i = temp_last; i > lastEleIndex; i--) {
+            arrLockSem.release();
         }
+
+        // signal writing finishes
+        signalFinishWriting(0, temp_last);
         
     }
 
-    private int binarySearch(int tar) {
+    // a binary search algo that adapted to search for an element in an array that has empty value indexes.
+    private int binarySearch(int tar) throws InterruptedException {
         if (numV == 0)
             return 0;
+
         int left = 0;
         int right = lastEleIndex;
+        signalWantReading(left, right);
         while (left <= right) {
             int mid = left + ((right - left) >> 1);
 
@@ -182,66 +287,49 @@ public class Ass1 {
 
             }
             if (arr.get(mid) == tar) {
+                signalFinishReading(left, mid - 1);
+                signalFinishReading(mid + 1, right);
                 return mid;
             } else if (tar < arr.get(mid)) {
+                int old_right = right;
                 right = mid - 1;
+                signalFinishReading(mid, old_right);
             } else {
+                int old_left = left;
                 left = mid + 1;
+                signalFinishReading(old_left, mid);
             }
         }
 
-        return left;
+        return left < N - 1 ? left : N - 1;
     }
 
-    public boolean member(int x) {
-        int result = this.binarySearch(x);
-    
-        if (result > lastEleIndex || result < 0) {
-            return false;
+    // signal a writer wants to write from fromIndex to toIndex
+    private void signalWantWriting(int fromIndex, int toIndex) throws InterruptedException {
+        for(int i = fromIndex; i <= toIndex; i++) {
+            semArr.get(i).startWriting();
         }
+    }
 
-        if (arr.get(result) != x) {
-            return false;
+    // signal a writer finshed writing from fromIndex to toIndex
+    private void signalFinishWriting(int fromIndex, int toIndex) throws InterruptedException {
+        for(int i = fromIndex; i <= toIndex; i++) {
+            semArr.get(i).finishWriting();
         }
-        return true;
     }
 
-    public void print_sorted() {
-        String result = "";
-        result = result + "{";
-        int index = 0;
-        for (int ele : arr) {
-            // if (ele == -1) {
-            //     continue;
-            // }
-            result = result + Integer.toString(ele);
-            if (index < lastEleIndex) {
-                result = result + ", ";
-            }
-            index++;
+    // signal a reader wants to read from fromIndex to toIndex
+    private void signalWantReading(int fromIndex, int toIndex) throws InterruptedException {
+        for(int i = fromIndex; i <= toIndex; i++) {
+            semArr.get(i).startReading();
         }
-        result = result + "}";
-        System.out.println(result);
     }
-    
 
-    public static void main(String[] args) {
-        Ass1 test = new Ass1(10);
-        numV = 0;
-        lastEleIndex = 0;
-        test.insert(8);
-        test.insert(10);
-        test.insert(9);
-        test.insert(1);
-
-        test.delete(8);
-        test.delete(10);
-
-        System.out.println(numV);
-        System.out.println(lastEleIndex);
-
-
-
-        test.print_sorted();
+    // signal a reader finshed writing from fromIndex to toIndex
+    private void signalFinishReading(int fromIndex, int toIndex) throws InterruptedException {
+        for(int i = fromIndex; i <= toIndex; i++) {
+            semArr.get(i).finishReading();
+        }
     }
+
 }
---
+++ MyTest.java
@@ -0,0 +1,165 @@
+public class MyTest {
+
+    // thread operations
+    public static class deleteThread extends Thread {
+        int deleteNum;
+
+        deleteThread(int Num) {
+            deleteNum = Num;
+        }
+
+        public void deleteOp(Ass1 test, int num) throws InterruptedException {
+            test.delete(num);
+        }
+
+        @Override
+        public void run() {
+            Ass1 newTest = new Ass1();
+            try {
+                deleteOp(newTest, deleteNum);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        }
+ 
+     }
+
+     public static class insertThread extends Thread {
+        int insertNum;
+
+        insertThread(int Num) {
+            insertNum = Num;
+        }
+
+        public void insertOp(Ass1 test, int num) throws InterruptedException {
+            test.insert(num);
+        }
+
+        @Override
+        public void run() {
+            Ass1 newTest = new Ass1();
+            try {
+                insertOp(newTest, insertNum);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        }
+ 
+     }
+
+     public static class memberThread extends Thread {
+        int target;
+
+        memberThread(int num) {
+            target = num;
+        }
+
+        public boolean memberOp(Ass1 test, int num) throws InterruptedException {
+            return test.member(num);
+        }
+
+        @Override
+        public void run() {
+            Ass1 newTest = new Ass1();
+            try {
+                if (memberOp(newTest, target)) {
+                    System.out.println("number found!");
+                } else {
+                    System.out.println("number not found!");
+                }
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        }
+     }
+
+
+    // test the array will have a fixed bound, i.e after inserted N number of elements, it will block the further insertion
+    // until deletions occur.
+
+    // test 1: test block case
+    public static void TestCaseFixBoundOne() throws InterruptedException {
+        Ass1 test = new Ass1();
+        test.insert(8);
+        test.insert(10);
+        test.insert(9);
+        test.insert(1);
+        test.insert(2);
+        test.insert(3); // this further insertion will be blocked
+    }
+
+    // test 2: test delete after block
+    public static void TestCaseFixBoundTwo() throws InterruptedException {
+        Ass1 test = new Ass1();
+        test.insert(8);
+        test.insert(10);
+        test.insert(9);
+        test.insert(1);
+        test.insert(2);
+        // this insertion will be blocked
+        insertThread insertThree = new insertThread(3);
+        insertThree.start();
+        
+        // another thread to delete existing element
+        deleteThread deletionEight = new deleteThread(8);
+        deletionEight.start();
+
+        // now the test can run to the end without getting blocked.
+        Thread.sleep(1000);
+        test.print_sorted();
+    }
+
+    // test the safety property of this implementation, 
+    // i.e read operations will never return false result when there is writing operation concurrently running
+    
+    // test: 
+    public static void TestCaseSaftey() throws InterruptedException {
+        Ass1 test = new Ass1();
+        test.insert(8);
+        test.insert(10);
+        test.insert(9);
+        test.insert(1);
+        test.insert(2);
+        test.delete(8);
+        
+        insertThread insertThree = new insertThread(11);
+        insertThree.start();
+
+        memberThread memberNine = new memberThread(9);
+        memberNine.start(); // this should print out "number found"
+    }
+
+    // test a more complex cases with multiple threads running the same time and doing different operations
+    public static void TestCaseComplex() throws InterruptedException {
+        Ass1 test = new Ass1();
+        test.insert(8);
+        test.insert(10);
+
+        // insert threads
+        for(int i = 1; i < 4; i++) {
+            insertThread insertNum = new insertThread(i);
+            insertNum.start();
+        }
+
+        // delete threads
+        for(int j = 1; j < 3; j++) {
+            deleteThread deleteNum = new deleteThread(j);
+            deleteNum.start();
+        }
+
+        // member threads, should all print "number found"
+        memberThread memberEight = new memberThread(8);
+        memberEight.start();
+        memberThread memberTen = new memberThread(10);
+        memberTen.start();
+
+        test.print_sorted(); // don't have to be the final array
+    }
+
+    public static void main(String[] args) throws InterruptedException {
+        // TestCaseFixBoundOne(); // this one should block
+        // TestCaseFixBoundTwo();
+        // TestCaseSaftey();
+        TestCaseComplex();
+    }
+}
